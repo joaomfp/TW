8b2db813cc714e8cc6417535b72e0fba022cb507
João Pereira <joao_mf_pereira@hotmail.com>
2017-10-28 00:58:02 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ var flag = 0; // 1 PC & 0 User
 var tam;
 var nim = 0;
 
-
 function gerar_tab() {
 	document.getElementById('tabuleiro').style.display = 'block';
 
@@ -19,14 +18,35 @@ function gerar_tab() {
 			cell_onclick(td, i, j);
 		}
 	}
-	if(flag!=0)
+	if (flag != 0)
 		setTimeout(pc_play, 100);
 
-	
 }
 
-function getCell(l, c)
-{
+function gerar_tab2() {
+	var table = document.getElementById('tabelaclassificacao');
+
+	for (var i = 0; i < 10; i++) {
+		var row = table.insertRow();
+		row.classList.add('tr_1');
+
+		for (var j = 0; j < 2; j++) {
+			var td = row.insertCell();
+			td.classList.add('td_1');
+
+		}
+	}
+	for ( i = 0; i < 10; i++) {
+		var player_class = i + 1 + 'º --------------';
+		console.log(player_class);
+			document.getElementById("tabelaclassificacao").rows[i].cells[1].innerHTML = player_class;
+		
+	}
+
+}
+
+
+function getCell(l, c) {
 	var boardElement = document.getElementById("tableclass");
 	return boardElement.rows[l].cells[c];
 }
@@ -36,62 +56,56 @@ function pc_play() {
 
 	var x, y;
 	var cell;
-	//alert(nim);
-	
-	if(nim <= 0)
+	// alert(nim);
+
+	if (nim <= 0)
 		return;
-	
-	do
-	{
-		x = Math.floor((Math.random() * tam) );
-		y = Math.floor((Math.random() * tam) );
-		//console.log(x + " " + y + " " + tam);
-	}
-	while(getCell(x, y).className != "td");
-	
-	
+
+	do {
+		x = Math.floor((Math.random() * tam));
+		y = Math.floor((Math.random() * tam));
+		// console.log(x + " " + y + " " + tam);
+	} while (getCell(x, y).className != "td");
+
 	for (var i = x; i >= 0; i--) {
 		var cell_vizinha = getCell(i, y);
-		if(cell_vizinha.className == "inactive")
+		if (cell_vizinha.className == "inactive")
 			break;
 		cell_vizinha.className = "temporario";
-		
-	
+
 	}
 
-	setTimeout(function(){for (var i = x; i >= 0; i--) {
-		var cell_vizinha = getCell(i, y);
-		if(cell_vizinha.className == "inactive")
-			break;
-		cell_vizinha.className = "inactive";
-		nim--;
-		
-	}
-	winner();
-	
-	flag = 0;},1000);
+	setTimeout(function() {
+		for (var i = x; i >= 0; i--) {
+			var cell_vizinha = getCell(i, y);
+			if (cell_vizinha.className == "inactive")
+				break;
+			cell_vizinha.className = "inactive";
+			nim--;
+
+		}
+		winner();
+
+		flag = 0;
+	}, 1000);
 }
 
-
-function winner()
-{
-	if(nim <= 0){
-		if(flag==0){
+function winner() {
+	if (nim <= 0) {
+		if (flag == 0) {
 			alert("Parabéns, ganhaste!");
-			
-		}
-		else{
+
+		} else {
 			alert("Perdeste, tente de novo!");
-			
+
 		}
-		
+
 		document.getElementById('tabuleiro').style.display = 'none';
 		document.getElementById('gerar_tab').style.display = 'none';
-		//document.getElementById('myFunction').style.display = 'none';
-		
-		
+		// document.getElementById('myFunction').style.display = 'none';
+
 		document.getElementById('try_again').style.display = 'block';
-		
+
 		return;
 	}
 }
@@ -99,32 +113,32 @@ function winner()
 function cell_onclick(cell, l, c) {
 
 	cell.onclick = function() {
-		if(flag == 0 && cell.className == "td")
-		{
+		if (flag == 0 && cell.className == "td") {
 			for (var i = l; i >= 0; i--) {
 				var cell_vizinha = getCell(i, c);
-				if(cell_vizinha.className == "inactive")
+				if (cell_vizinha.className == "inactive")
 					break;
-				
+
 				cell_vizinha.className = "inactive";
 				nim--;
 
 			}
-			//nim = nim - (l + 1);
-			
+			// nim = nim - (l + 1);
+
 			winner();
-			flag = 1;	
+			flag = 1;
 			setTimeout(pc_play, 3);
 		}
 	}
 
 	cell.onmouseover = function() {
 		if (cell.className !== "inactive" && cell.className !== "temporario")
-			for (var i = l-1 ; i >= 0; i--) {
+			for (var i = l - 1; i >= 0; i--) {
 				var cell_vizinha = getCell(i, c);
-				if (cell_vizinha.className == "inactive" || cell_vizinha.className == "temporario")
+				if (cell_vizinha.className == "inactive"
+						|| cell_vizinha.className == "temporario")
 					break;
-					cell_vizinha.className = "active";
+				cell_vizinha.className = "active";
 			}
 	}
 
@@ -132,7 +146,8 @@ function cell_onclick(cell, l, c) {
 		if (cell.className !== "inactive" && cell.className !== "temporario")
 			for (var i = l; i >= 0; i--) {
 				var cell_vizinha = getCell(i, c);
-				if (cell_vizinha.className == "inactive" || cell_vizinha.className == "temporario")
+				if (cell_vizinha.className == "inactive"
+						|| cell_vizinha.className == "temporario")
 					break;
 				cell_vizinha.className = "td";
 			}
@@ -140,14 +155,21 @@ function cell_onclick(cell, l, c) {
 
 }
 
-function clearBoard()
-{
-    
-    var i;    
-    for(i = 0; i < tam; i++)
-    {
-        document.getElementById("tableclass").deleteRow(0);
-    }   
+function clearBoard() {
+
+	var i;
+	for (i = 0; i < tam; i++) {
+		document.getElementById("tableclass").deleteRow(0);
+	}
+}
+function clearBoard_rank() {
+
+	var i;
+	for (i = 0; i < 10; i++) {
+		if (document.getElementById("tabelaclassificacao").rows.length != 0)
+			document.getElementById("tabelaclassificacao").deleteRow(0);
+
+	}
 }
 
 function myFunction() {
@@ -163,15 +185,15 @@ function myFunction() {
 function first_play() {
 	document.getElementById('demo').style.display = 'none';
 	document.getElementById('demo_2').style.display = 'block';
-	
-	 flag=0//user
+
+	flag = 0// user
 
 }
 function first_play_pc() {
 	document.getElementById('demo').style.display = 'none';
 	document.getElementById('demo_2').style.display = 'block';
 
-	flag= 1; //pc
+	flag = 1; // pc
 
 }
 
@@ -192,19 +214,22 @@ function login_wData() {
 	document.getElementById('painel_login').style.display = 'none';
 }
 function config() {
-	clearBoard();
 	document.getElementById('painel_login').style.display = 'none';
 	document.getElementById('painel_rank').style.display = 'none';
 	document.getElementById('painel_init').style.display = 'none';
 	document.getElementById('painel_instrucoes').style.display = 'none';
+	document.getElementById('gerar_tab').style.display = 'block';
 	document.getElementById('painel_jogo').style.display = 'block';
+	document.getElementById('tabuleiro').style.display = 'block';
+
 }
-function modo_jogo(){
+function modo_jogo() {
 	document.getElementById('modo_jogo').style.display = 'none';
 	document.getElementById('demo').style.display = 'block';
 }
 function instrucoes() {
-	document.getElementById('modo_jogo').style.display = 'none';
+	document.getElementById('gerar_tab').style.display = 'none';
+	document.getElementById('painel_jogo').style.display = 'none';
 	document.getElementById('painel_login').style.display = 'none';
 	document.getElementById('painel_init').style.display = 'none';
 	document.getElementById('painel_rank').style.display = 'none';
@@ -212,22 +237,26 @@ function instrucoes() {
 
 }
 function rank() {
+	document.getElementById('gerar_tab').style.display = 'none';
 	document.getElementById('painel_jogo').style.display = 'none';
 	document.getElementById('painel_login').style.display = 'none';
 	document.getElementById('painel_init').style.display = 'none';
 	document.getElementById('painel_instrucoes').style.display = 'none';
+	document.getElementById('gerar_tab').style.display = 'none';
+	clearBoard_rank();
 	document.getElementById('painel_rank').style.display = 'block';
+	gerar_tab2();
+	document.getElementById('tabelaclassificacao').style.display = 'block';
+
 }
 
-function home(){
+function home() {
+	document.getElementById('gerar_tab').style.display = 'none';
 	document.getElementById('painel_jogo').style.display = 'none';
-	document.getElementById('painel_instrucoes').style.display = 'none'; 
+	document.getElementById('painel_instrucoes').style.display = 'none';
 	document.getElementById('painel_login').style.display = 'none';
 	document.getElementById('painel_rank').style.display = 'none';
-	document.getElementById('modo_jogo').style.display = 'none';
 	document.getElementById('painel_init').style.display = 'block';
-
-
 
 }
 function table_click() {
@@ -235,12 +264,11 @@ function table_click() {
 	alert(boardElement.rows[l].cells[c]);
 }
 
-function try_again(){
+function try_again() {
 	clearBoard();
 	document.getElementById('try_again').style.display = 'none';
 	document.getElementById('demo').style.display = 'block';
-	
-	//myFunction();
-	
+
+	// myFunction();
 
 }
